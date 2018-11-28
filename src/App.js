b7cf139a2eed574779274de27967e2d1481ff3b0
Paul Williams <paul.williams@blackpepper.co.uk>
2018-11-28 13:10:41 +0000

    Move Apollo Provider to index.js, and externalise users query

--- src/App.js
+++ src/App.js
@@ -1,49 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {ApolloProvider} from "react-apollo";
-import {RestLink} from "apollo-link-rest";
-import {ApolloClient} from "apollo-client";
-import {InMemoryCache} from "apollo-cache-inmemory";
 import Users from "./Users";
+import { graphql } from 'react-apollo';
+import { loader } from 'graphql.macro';
 
-const restLink = new RestLink({
-  uri: 'http://localhost:8080',
-});
+const query = loader('./Users.graphql');
 
-const client = new ApolloClient({
-  link: restLink,
-  cache: new InMemoryCache(),
-});
-
-class App extends Component {
-  render() {
-    return (
+function App(props) {
+  const {data: {users = []}} = props;
+  return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
           >
             Learn React
           </a>
         </header>
-        <Users/>
+        <Users users={users}/>
       </div>
-    );
-  }
+  );
 }
 
-const ApolloApp = () => (
-    <ApolloProvider client={client}>
-      <App/>
-    </ApolloProvider>
-);
+export default graphql(query)(App);
 
-export default ApolloApp;
+
+
--- src/Users.js
+++ src/Users.js
@@ -1,35 +1,12 @@
-import React, {PureComponent} from "react";
-import gql from "graphql-tag";
-import { Query } from "react-apollo";
+import React from "react";
 import './Users.scss';
 
-const USERS_QUERY = gql`
-  query userQuery {
-    users @rest(type: "Users", path: "/users") {
-      id
-      firstName
-      lastName
-    }
-  }
-`;
-
-class Users extends PureComponent {
-  render() {
-    return (
-        <Query query={USERS_QUERY}>
-          {({ loading, error, data }) => {
-            if (loading) return "Loading...";
-            if (error) return `Error! ${error.message}`;
-
-            return (
-                data.users.map(user => (
-                    <li key={user.id}>{user.firstName} {user.lastName}</li>
-                ))
-            );
-          }}
-        </Query>
-    )
-  }
+function Users(props) {
+  return (
+      props.users.map(user => (
+          <li key={user.id}>{user.firstName} {user.lastName}</li>
+      ))
+  )
 }
 
 export default Users;
